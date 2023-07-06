26dc45f347907a9451d03fbe70b99bc313d5205d
Zuhair AlSader <zuhair@koor.tech>
2023-07-06 17:37:24 -0400

    feat: add basic server
    Signed-off-by: Zuhair AlSader <zuhair@koor.tech>

--- go.mod
+++ go.mod
@@ -5,5 +5,8 @@ go 1.19
 require (
 	github.com/bufbuild/connect-go v1.9.0
 	github.com/envoyproxy/protoc-gen-validate v0.10.1
+	golang.org/x/net v0.8.0
 	google.golang.org/protobuf v1.31.0
 )
+
+require golang.org/x/text v0.8.0 // indirect
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"net/http"
+
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
+
+	"github.com/koor-tech/version-service/api/v1/apiv1connect"
+	serverv1 "github.com/koor-tech/version-service/server/v1"
+)
+
+func main() {
+	greeter := &serverv1.VersionServer{}
+	mux := http.NewServeMux()
+	path, handler := apiv1connect.NewVersionServiceHandler(greeter)
+	mux.Handle(path, handler)
+	http.ListenAndServe(
+		"localhost:8080",
+		// Use h2c so we can serve HTTP/2 without TLS.
+		h2c.NewHandler(mux, &http2.Server{}),
+	)
+}
