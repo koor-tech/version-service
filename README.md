7301adaced0844c59d0dc0e0cdd90d493bbb2722
Zuhair AlSader <zuhair@koor.tech>
2023-07-21 10:42:19 -0400

    feat: add docker image and k8s (#4)

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# version-service
-Version service for KSD
+# KSD Version Service
+KSD Version service provides an API to access the supported versions of KSD 
+and its dependencies.
+
+For each version of the operator, there is a file in the `data` directory that 
+contains a list of supported components for that version.
+
+## Running locally
+```bash
+make run
+```
--- data/embed.go
+++ data/embed.go
@@ -14,26 +14,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package main
+package data
 
-import (
-	"net/http"
+import "embed"
 
-	"golang.org/x/net/http2"
-	"golang.org/x/net/http2/h2c"
-
-	"github.com/koor-tech/version-service/api/v1/apiv1connect"
-	serverv1 "github.com/koor-tech/version-service/server/v1"
-)
-
-func main() {
-	vs := &serverv1.VersionServer{}
-	mux := http.NewServeMux()
-	path, handler := apiv1connect.NewVersionServiceHandler(vs)
-	mux.Handle(path, handler)
-	http.ListenAndServe(
-		":8082",
-		// Use h2c so we can serve HTTP/2 without TLS.
-		h2c.NewHandler(mux, &http2.Server{}),
-	)
-}
+//go:embed *.json
+var Data embed.FS
